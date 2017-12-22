3492bdb1fad42334e25988b1179ad593c4519a0c
renuraj <renuraj18@gmail.com>
2017-12-22 12:26:56 +0000

    added boot script

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ RUN echo 2.60.3 > /usr/share/jenkins/ref/jenkins.install.UpgradeWizard.state
 RUN echo 2.60.3 > /usr/share/jenkins/ref/jenkins.install.InstallUtil.lastExecVersion
 
 COPY plugins.txt /usr/share/jenkins/ref/
-COPY adop_ldap.groovy /usr/share/jenkins/ref/init.groovy.d/
-COPY role_based_auth.groovy /usr/share/jenkins/ref/init.groovy.d/
+COPY *.groovy /usr/share/jenkins/ref/init.groovy.d/
+COPY jobs/ /usr/share/jenkins/ref/jobs/
 RUN /usr/local/bin/plugins.sh /usr/share/jenkins/ref/plugins.txt
 
 ENV ADOP_LDAP_ENABLED=true LDAP_IS_MODIFIABLE=true ADOP_ACL_ENABLED=true
---
+++ kube_cloud_config.groovy
@@ -0,0 +1,27 @@
+import org.csanchez.jenkins.plugins.kubernetes.*
+import jenkins.model.*
+
+// Variables
+def env = System.getenv()
+def kube_master = env['KUBERNETES_SERVICE_HOST']
+def kube_port = env['KUBERNETES_SERVICE_PORT']
+
+def instance = Jenkins.getInstance()
+
+//Plugin configuration
+def name = "kubernetes"
+def kube_url = "https://"+kube_master+":"+kube_port
+def kube_ns = "renu"
+def jenkins_url = "http://jenkins-blueocean.renu.svc.cluster.local:8080/jenkins"
+def containerCapStr = "10"
+def connectTimeout = 0
+def readTimeout = 0
+def retentionTimeout = 5
+def maxRequestsPerHost = "32"
+
+def kc =  new KubernetesCloud( name, null, kube_url, kube_ns, jenkins_url, containerCapStr, connectTimeout, readTimeout, retentionTimeout)
+
+kc.setSkipTlsVerify(true)
+kc.setMaxRequestsPerHostStr(maxRequestsPerHost)
+instance.clouds.replace(kc)
+instance.save()
